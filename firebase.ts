a799527eb76fa0054ada5a2d62a47f5d2ab91797
Zalswaw <121928926+Zalswaw@users.noreply.github.com>
2026-01-22 19:20:43 +0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,21 @@
-
-const FIREBASE_HOST = "iot-project.firebaseio.com"
+let firebaseHost = ""
+let firebaseAuth = ""
 
 namespace esp8266 {
 
     let firebaseUploadSuccess = false
+
+    //% subcategory="Firebase"
+    //% block="Set Firebase Host %host"
+    export function setFirebaseHost(host: string) {
+        firebaseHost = host
+    }
+
+    //% subcategory="Firebase"
+    //% block="Set Firebase Token %token"
+    export function setFirebaseToken(token: string) {
+        firebaseAuth = token
+    }
 
     //% subcategory="Firebase"
     //% block="Firebase upload success"
@@ -12,47 +24,40 @@ namespace esp8266 {
     }
 
     //% subcategory="Firebase"
-    //% block="Send to Firebase|Path %path|Data %data"
+    //% block="Send to Firebase Path %path Data %data"
     export function sendToFirebase(path: string, data: string) {
 
         firebaseUploadSuccess = false
 
         if (!isWifiConnected()) return
 
-        // Connect SSL
         if (!sendCommand(
-            "AT+CIPSTART=\"SSL\",\"" + FIREBASE_HOST + "\",443",
+            "AT+CIPSTART=\"SSL\",\"" + firebaseHost + "\",443",
             "OK",
             10000
         )) return
 
-        // JSON body
         let body = data
 
-        // HTTP Request
-        let request = "PUT /" + path + ".json HTTP/1.1\r\n"
-        request += "Host: " + FIREBASE_HOST + "\r\n"
+        let request = "PUT /" + path + ".json"
+
+        if (firebaseAuth != "") {
+            request += "?auth=" + firebaseAuth
+        }
+
+        request += " HTTP/1.1\r\n"
+
+        request += "Host: " + firebaseHost + "\r\n"
         request += "Content-Type: application/json\r\n"
         request += "Content-Length: " + body.length + "\r\n\r\n"
         request += body
 
-        // Send
         sendCommand("AT+CIPSEND=" + request.length)
         sendCommand(request)
 
-        // Check send
-        if (getResponse("SEND OK", 2000) == "") {
-            sendCommand("AT+CIPCLOSE", "OK", 1000)
-            return
-        }
+        if (getResponse("SEND OK", 2000) == "") return
 
-        // Check Firebase response
-        let res = getResponse("200 OK", 3000)
-
-        if (res == "") {
-            sendCommand("AT+CIPCLOSE", "OK", 1000)
-            return
-        }
+        if (getResponse("200 OK", 3000) == "") return
 
         sendCommand("AT+CIPCLOSE", "OK", 1000)
 
