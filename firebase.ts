a28ab65ee05441fd80eabfdb528704afd13a4937
Zalswaw <121928926+Zalswaw@users.noreply.github.com>
2026-01-29 10:44:48 +0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -6,7 +6,7 @@ namespace esp8266 {
     let uploadSuccess = false
 
     //============================
-    // SET SERVER HOST / DOMAIN
+    // SET SERVER HOST
     //============================
 
     //% subcategory="Server"
@@ -20,14 +20,14 @@ namespace esp8266 {
     }
 
     //============================
-    // SET SERVER PATH (PHP FILE)
+    // SET SERVER PATH
     //============================
 
     //% subcategory="Server"
     //% block="Set Server Path %path"
     export function setServerPath(path: string) {
 
-        if (!path.startsWith("/")) {
+        if (path.charAt(0) != "/") {
             path = "/" + path
         }
 
@@ -35,7 +35,7 @@ namespace esp8266 {
     }
 
     //============================
-    // STATUS UPLOAD
+    // UPLOAD STATUS
     //============================
 
     //% subcategory="Server"
@@ -45,7 +45,7 @@ namespace esp8266 {
     }
 
     //============================
-    // SEND DATA TO SERVER
+    // SEND TO SERVER
     //============================
 
     //% subcategory="Server"
@@ -54,14 +54,10 @@ namespace esp8266 {
 
         uploadSuccess = false
 
-        // Check WiFi
         if (!isWifiConnected()) return
-
-        // Check Host
         if (serverHost == "") return
 
 
-        // Connect TCP (HTTP PORT 80)
         if (!sendCommand(
             "AT+CIPSTART=\"TCP\",\"" + serverHost + "\",80",
             "OK",
@@ -69,41 +65,32 @@ namespace esp8266 {
         )) return
 
 
-        // Encode data
         let safeData = formatUrl(data)
 
-        // Build URL
         let url = serverPath + "?path=" + path + "&data=" + safeData
 
 
-        // HTTP GET Request
         let request = "GET " + url + " HTTP/1.1\r\n"
         request += "Host: " + serverHost + "\r\n"
         request += "Connection: close\r\n\r\n"
 
 
-        // Send Length
         sendCommand("AT+CIPSEND=" + request.length)
-
-        // Send Request
         sendCommand(request)
 
 
-        // Check Send OK
         if (getResponse("SEND OK", 3000) == "") {
             sendCommand("AT+CIPCLOSE", "OK", 1000)
             return
         }
 
 
-        // Check HTTP 200
         if (getResponse("200 OK", 5000) == "") {
             sendCommand("AT+CIPCLOSE", "OK", 1000)
             return
         }
 
 
-        // Close Connection
         sendCommand("AT+CIPCLOSE", "OK", 1000)
 
         uploadSuccess = true
